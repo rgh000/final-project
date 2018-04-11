0338a95be82111d048bf504dcc64a4de85f69de3
rgh000 <35755112+rgh000@users.noreply.github.com>
2018-04-11 00:12:33 -0400

    Create project.py

---
+++ project.py
@@ -0,0 +1,161 @@
+import scipy
+import scipy.sparse
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.stats import pearsonr
+import math
+import random
+import re
+import numpy as np
+
+from gensim.models import KeyedVectors
+model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+
+def alpha(word):
+	return re.sub('[^A-Za-z\']+', '', word.lower())
+
+f = open('fwlist-277.txt', 'r')
+lines4 = f.readlines()
+f.close()
+functions = []
+for l in lines4:
+	ls = l.split()
+	functions.append(alpha(ls[0]))
+
+f = open('lemma.al', 'r')
+lines2 = f.readlines()
+f.close()
+targets = dict()
+i = 0
+for l in lines2:
+	ls = l.split()
+	if not ls[2] in targets:
+		targets[ls[2]] = i
+		i += 1
+
+
+f = open('hunspell/en_US.dic', 'r')
+lines3 = f.readlines()
+f.close()
+contexts = dict()
+i = 0
+for l in lines3:
+	ls = l.split()[0].split('/')
+	if alpha(ls[0]) == ls[0] and not ls[0] in contexts:
+		contexts[ls[0]] = i
+		i += 1
+
+
+def word2vec(word):
+	try:
+		a = model[alpha(word)]
+		return True
+	except:
+		return False
+
+
+def revcont(n):
+	for k,v in contexts.iteritems():
+		if v == n:
+			return k
+
+M_list = []
+for i in range(20):
+	M_list.append(scipy.sparse.load_npz('M_M' + str(i) + '.npz').tolil())
+
+
+target_word = 'mouse'
+year = 18
+
+lil = M_list[year][targets[target_word]].tolil()
+
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+page = urllib2.urlopen('http://historicalthesaurus.arts.gla.ac.uk/category-selection/?qsearch=' + target_word)
+soup = BeautifulSoup(page, 'html.parser')
+box = soup.find_all('p', class_='catOdd') + soup.find_all('p', class_='catEven')
+definitions = []
+for b in box:
+	if not 'Matching word' in b.text and b.text[-1:] == ')':
+		definitions.append(re.sub('[0-9a-z| ]*\.', '', b.text).replace(u'\u2013', '-').replace('/', ' ').split(' :: '))
+
+defv = []
+for d in definitions:
+	#if d[len(d)-1][-2:-1] == '-' or (d[len(d)-1][-2:-1].isdigit() and int(d[len(d)-1][-5:-1]) > 1800 and d[len(d)-1][-6:-5] == '-'):
+	if d[len(d)-1][-2:-1] == '-' and (d[len(d)-1][-4:-2] == 'OE' or (d[len(d)-1][-3:-2].isdigit() and d[len(d)-1][-4:-3].isdigit() and d[len(d)-1][-5:-4].isdigit() and d[len(d)-1][-6:-5].isdigit() and int(d[len(d)-1][-6:-2]) < (1809 + year * 10))):
+		l = np.zeros(300)
+		count = 0
+		for e in d:
+			f = re.split(r'[ /]+', e)
+			for g in f:
+				if not alpha(g) == '' and not alpha(g) in functions and word2vec(g):
+					l += model[alpha(g)]
+					count += 1
+		defv.append(l / (count + 1e-15))
+
+############ WARNING: NO IDEA IF THIS WORKS!
+cluster = []
+prev = []
+vectors = []
+for d in defv:
+	vectors.append([d])
+
+
+for z in range(10):
+	print z
+	mean = []
+	for v in vectors:
+		mean.append(sum(v) / (1e-15 + len(v)))
+	vectors = []
+	cluster = []
+	for d in defv:
+		vectors.append([d])
+		cluster.append([])
+	for i, (row, data) in enumerate(zip(lil.rows, lil.data)):
+		for j, val in zip(row, data):
+			count = 0
+			avg = np.zeros(300)
+			fourth = j / (len(contexts)*len(contexts)*len(contexts))
+			third = (j - fourth*len(contexts)*len(contexts)*len(contexts)) / (len(contexts)*len(contexts))
+			second = (j - fourth*len(contexts)*len(contexts)*len(contexts) - third*len(contexts)*len(contexts)) / len(contexts)
+			first = j - fourth*len(contexts)*len(contexts)*len(contexts) - third*(len(contexts)*len(contexts)) - second*len(contexts)
+			if word2vec(revcont(fourth)) and alpha(revcont(fourth)) not in functions:
+				avg = avg + model[alpha(revcont(fourth))]
+				count += 1
+			if word2vec(revcont(third)) and alpha(revcont(third)) not in functions:
+				avg = avg + model[alpha(revcont(third))]
+				count += 1
+			if word2vec(revcont(second)) and alpha(revcont(second)) not in functions:
+				avg = avg + model[alpha(revcont(second))]
+				count += 1
+			if word2vec(revcont(first)) and alpha(revcont(first)) not in functions:
+				avg = avg + model[alpha(revcont(first))]
+				count += 1
+			if not count == 0:
+			#if True:
+				avg = avg / (count + 1e-15)
+				maximum = -1000000
+				maxi = -1000000
+				for i in range(len(defv)):
+					if cosine_similarity([avg, mean[i]])[0][1] > maximum:
+						maximum = cosine_similarity([avg, mean[i]])[0][1]
+						maxi = i
+				for q in range(int(val)):
+				#if True:
+					cluster[maxi].append('{' + alpha(revcont(first)) + ' ' + alpha(revcont(second)) + ' ' + target_word + ' ' + alpha(revcont(third)) + ' ' + alpha(revcont(fourth)) + '}')
+					vectors[maxi].append(avg)
+	if cluster == prev:
+		break
+	else:
+		prev = cluster
+
+i = -1
+for d in definitions:
+	#if d[len(d)-1][-2:-1] == '-' or (d[len(d)-1][-2:-1].isdigit() and int(d[len(d)-1][-5:-1]) > 1800 and d[len(d)-1][-6:-5] == '-'):
+	if d[len(d)-1][-2:-1] == '-' and (d[len(d)-1][-4:-2] == 'OE' or (d[len(d)-1][-3:-2].isdigit() and d[len(d)-1][-4:-3].isdigit() and d[len(d)-1][-5:-4].isdigit() and d[len(d)-1][-6:-5].isdigit() and int(d[len(d)-1][-6:-2]) < (1809 + year * 10))):
+		i += 1
+		print d#, 'OE' if d[len(d)-1][-4:-2] == 'OE' else (int(d[len(d)-1][-6:-2]) if d[len(d)-1][-2:-1] == '-' else int(d[len(d)-1][-5:-1]))
+		print "Cluster: ", set(cluster[i])
+		print ''
